348c170c388dbec80e8395608ff2b65ca3dec0b3
bradpickard <brad.pickard12@gmail.com>
2021-09-25 02:46:19 -0700

    Initial Commit

---
+++ Test Cognito/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Test Cognito
+//
+//  Created by Web Dev on 9/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Test Cognito/Test_CognitoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CognitoApp.swift
+//  Test Cognito
+//
+//  Created by Web Dev on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct Test_CognitoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
